f28c37224a165bc14eafce7b223754b4bbbd4419
Alex <AlexTheProgrammerMail@gmail.com>
2014-10-08 19:37:06 -0700

    Some refactoring done.

---
+++ src/com/alex/resumeDicer/Model/AccountInfoModel.java
@@ -0,0 +1,12 @@
+package com.alex.resumeDicer.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AccountInfoModel {
+
+	private String emailAdress;
+	private String password;
+}
--- src/com/alex/resumeDicer/UI/AccountInfo.java
+++ src/com/alex/resumeDicer/UI/AccountInfo.java
@@ -9,9 +9,12 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 
+import com.alex.resumeDicer.Model.AccountInfoModel;
+
 public class AccountInfo extends Composite {
-	private Text text;
-	private Text text_1;
+
+	private Text emailAddress;
+	private Text password;
 
 	/**
 	 * Create the composite.
@@ -29,12 +32,12 @@ public class AccountInfo extends Composite {
 		lblAccountEmailAddress.setLayoutData(fd_lblAccountEmailAddress);
 		lblAccountEmailAddress.setText("Account email address:");
 
-		text = new Text(this, SWT.BORDER);
-		FormData fd_text = new FormData();
-		fd_text.right = new FormAttachment(0, 183);
-		fd_text.top = new FormAttachment(lblAccountEmailAddress, 6);
-		fd_text.left = new FormAttachment(0, 10);
-		text.setLayoutData(fd_text);
+		emailAddress = new Text(this, SWT.BORDER);
+		FormData fd_emailAddress = new FormData();
+		fd_emailAddress.right = new FormAttachment(0, 183);
+		fd_emailAddress.top = new FormAttachment(lblAccountEmailAddress, 6);
+		fd_emailAddress.left = new FormAttachment(0, 10);
+		emailAddress.setLayoutData(fd_emailAddress);
 
 		Label lblPassword = new Label(this, SWT.NONE);
 		FormData fd_lblPassword = new FormData();
@@ -42,18 +45,18 @@ public class AccountInfo extends Composite {
 		lblPassword.setLayoutData(fd_lblPassword);
 		lblPassword.setText("Password:");
 
-		text_1 = new Text(this, SWT.BORDER | SWT.PASSWORD);
-		fd_lblPassword.left = new FormAttachment(text_1, 0, SWT.LEFT);
-		FormData fd_text_1 = new FormData();
-		fd_text_1.top = new FormAttachment(lblPassword, 6);
-		fd_text_1.right = new FormAttachment(text, 179, SWT.RIGHT);
-		fd_text_1.left = new FormAttachment(text, 6);
-		text_1.setLayoutData(fd_text_1);
+		password = new Text(this, SWT.BORDER | SWT.PASSWORD);
+		fd_lblPassword.left = new FormAttachment(password, 0, SWT.LEFT);
+		FormData fd_password = new FormData();
+		fd_password.top = new FormAttachment(lblPassword, 6);
+		fd_password.right = new FormAttachment(emailAddress, 179, SWT.RIGHT);
+		fd_password.left = new FormAttachment(emailAddress, 6);
+		password.setLayoutData(fd_password);
 
 		Button btnNewButton = new Button(this, SWT.NONE);
 		FormData fd_btnNewButton = new FormData();
-		fd_btnNewButton.top = new FormAttachment(text, -2, SWT.TOP);
-		fd_btnNewButton.left = new FormAttachment(text_1, 6);
+		fd_btnNewButton.top = new FormAttachment(emailAddress, -2, SWT.TOP);
+		fd_btnNewButton.left = new FormAttachment(password, 6);
 		btnNewButton.setLayoutData(fd_btnNewButton);
 		btnNewButton.setText("Remove");
 
@@ -62,5 +65,14 @@ public class AccountInfo extends Composite {
 	@Override
 	protected void checkSubclass() {
 		// Disable the check that prevents subclassing of SWT components
+	}
+
+	public void setAccountInfo(AccountInfoModel model) {
+		this.emailAddress.setText(model.getEmailAdress());
+		this.password.setText(model.getPassword());
+	}
+
+	public AccountInfoModel getAccountInfoModel() {
+		return new AccountInfoModel(emailAddress.getText(), password.getText());
 	}
 }
