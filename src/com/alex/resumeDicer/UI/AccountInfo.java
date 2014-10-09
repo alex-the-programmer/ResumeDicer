fd2e880bc00ddb95abfb07f7cb9f8d607e9e6465
Alex <AlexTheProgrammerMail@gmail.com>
2014-10-08 17:04:24 -0700

    AccountInfo controll added.

---
+++ src/com/alex/resumeDicer/UI/AccountInfo.java
@@ -0,0 +1,22 @@
+package com.alex.resumeDicer.UI;
+
+import org.eclipse.swt.widgets.Composite;
+
+public class AccountInfo extends Composite {
+
+	/**
+	 * Create the composite.
+	 * @param parent
+	 * @param style
+	 */
+	public AccountInfo(Composite parent, int style) {
+		super(parent, style);
+
+	}
+
+	@Override
+	protected void checkSubclass() {
+		// Disable the check that prevents subclassing of SWT components
+	}
+
+}
